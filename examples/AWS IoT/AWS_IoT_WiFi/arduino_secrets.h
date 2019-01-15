085ce90d66a700db2496e23b9bbb7945c93abada
Sandeep Mistry <s.mistry@arduino.cc>
2019-01-15 11:14:33 -0500

    Add AWS IoT WiFi example

---
+++ examples/AWS IoT/AWS_IoT_WiFi/arduino_secrets.h
@@ -0,0 +1,13 @@
+// Fill in  your WiFi networks SSID and password
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+
+// Fill in the hostname of your AWS IoT broker
+#define SECRET_BROKER "xxxxxxxxxxxxxx.iot.xx-xxxx-x.amazonaws.com"
+
+// Fill in the boards public certificate
+const char SECRET_CERTIFICATE[] = R"(
+-----BEGIN CERTIFICATE-----
+
+-----END CERTIFICATE-----
+)";
