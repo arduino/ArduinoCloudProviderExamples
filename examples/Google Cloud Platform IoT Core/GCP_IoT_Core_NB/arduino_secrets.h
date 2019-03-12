cc16e554503b2b293515363ae7fb516a54427f80
Sandeep Mistry <s.mistry@arduino.cc>
2019-03-12 17:13:40 -0400

    Add GCP IoT Core WiFi, GSM, and NB examples

---
+++ examples/Google Cloud Platform IoT Core/GCP_IoT_Core_NB/arduino_secrets.h
@@ -0,0 +1,8 @@
+// NB settings
+#define SECRET_PINNUMBER ""
+
+// Fill in your Google Cloud Platform - IoT Core info
+#define SECRET_PROJECT_ID   ""
+#define SECRET_CLOUD_REGION ""
+#define SECRET_REGISTRY_ID  ""
+#define SECRET_DEVICE_ID    ""
---
+++ examples/Google Cloud Platform IoT Core/GCP_IoT_Core_WiFi/arduino_secrets.h
@@ -0,0 +1,9 @@
+// Fill in your WiFi networks SSID and password
+#define SECRET_SSID         ""
+#define SECRET_PASS         ""
+
+// Fill in your Google Cloud Platform - IoT Core info
+#define SECRET_PROJECT_ID   ""
+#define SECRET_CLOUD_REGION ""
+#define SECRET_REGISTRY_ID  ""
+#define SECRET_DEVICE_ID    ""
