1188a7210caa39bb0153c59680d5b5f4ae10e4dc
pennam <m.pennasilico@arduino.cc>
2024-11-08 17:05:26 +0100

    Add example for Giga using WiFiSSLClient

---
+++ examples/AWS IoT/AWS_IoT_Giga_WiFi/arduino_secrets.h
@@ -0,0 +1,20 @@
+// Fill in  your WiFi networks SSID and password
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+
+// Fill in the hostname of your AWS IoT broker
+#define SECRET_BROKER "anw14dfe9dd1u-ats.iot.eu-north-1.amazonaws.com"
+#define SECRET_PORT 8883
+
+// Fill in the board public certificate
+const char SECRET_CERTIFICATE[] = R"(
+-----BEGIN CERTIFICATE-----
+-----END CERTIFICATE-----
+)";
+
+// Fill in the board private key
+const char PRIVATE_KEY[] = R"(
+-----BEGIN EC PRIVATE KEY-----
+-----END EC PRIVATE KEY-----
+)";
+
